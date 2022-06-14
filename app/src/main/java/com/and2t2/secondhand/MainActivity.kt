1a945c303f5d0feee1b7972d8502131fa078a97e
Aji Fauzi P <ajifauzi919@gmail.com>
2022-06-14 14:40:23 +0700

    first commit

---
+++ app/src/main/java/com/and2t2/secondhand/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.and2t2.secondhand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
