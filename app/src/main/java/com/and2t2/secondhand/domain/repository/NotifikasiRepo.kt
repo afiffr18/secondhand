85ff62b01a33f642d11fdeea3416a0dcdd11aa41
melur.puspita1 <melur.puspita1@gmail.com>
2022-06-22 11:10:52 +0700

    Memperbaiki error

--- app/src/main/java/com/and2t2/secondhand/domain/repository/NotifikasiRepo.kt
+++ app/src/main/java/com/and2t2/secondhand/domain/repository/NotifikasiRepo.kt
@@ -2,39 +2,39 @@ package com.and2t2.secondhand.domain.repository
 
 import androidx.room.withTransaction
 import com.and2t2.secondhand.common.networkBoundResource
-import com.and2t2.secondhand.data.local.DatabaseSecondHand
+//import com.and2t2.secondhand.data.local.DatabaseSecondHand
 import com.and2t2.secondhand.data.remote.NotifikasiService
 import com.and2t2.secondhand.domain.model.Notifikasi
 import com.and2t2.secondhand.domain.model.NotifikasiMapper
 
-class NotifikasiRepo(
-    private val apiService : NotifikasiService,
-    private val mapper: NotifikasiMapper,
-    private val mDb : DatabaseSecondHand
-) {
-    suspend fun getNotifikasi(access_token : String) : List<Notifikasi>{
-        val result = apiService.getNotification(access_token)
-        return mapper.toDomainList(result)
-    }
-
-    private val notifikasiDao = mDb.notifikasiDao()
-
-  fun getNotif(access_token: String) = networkBoundResource(
-        query = {
-            notifikasiDao.getNotifikasi()
-        },
-        fetch = {
-            getNotifikasi(access_token)
-        },
-        saveFetchResult = { notif ->
-            mDb.withTransaction{
-                notifikasiDao.deleteNotifikasi()
-                notifikasiDao.insertNotifikasi(notif)
-            }
-
-
-        }
-
-    )
-
-}
+//class NotifikasiRepo(
+//    private val apiService : NotifikasiService,
+//    private val mapper: NotifikasiMapper,
+//    private val mDb : DatabaseSecondHand
+//) {
+//    suspend fun getNotifikasi(access_token : String) : List<Notifikasi>{
+//        val result = apiService.getNotification(access_token)
+//        return mapper.toDomainList(result)
+//    }
+//
+//    private val notifikasiDao = mDb.notifikasiDao()
+//
+//  fun getNotif(access_token: String) = networkBoundResource(
+//        query = {
+//            notifikasiDao.getNotifikasi()
+//        },
+//        fetch = {
+//            getNotifikasi(access_token)
+//        },
+//        saveFetchResult = { notif ->
+//            mDb.withTransaction{
+//                notifikasiDao.deleteNotifikasi()
+//                notifikasiDao.insertNotifikasi(notif)
+//            }
+//
+//
+//        }
+//
+//    )
+//
+//}
--- app/src/main/java/com/and2t2/secondhand/ui/uinotifikasi/NotifikasiViewModel.kt
+++ app/src/main/java/com/and2t2/secondhand/ui/uinotifikasi/NotifikasiViewModel.kt
@@ -2,11 +2,11 @@ package com.and2t2.secondhand.ui.uinotifikasi
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.and2t2.secondhand.domain.repository.NotifikasiRepo
+//import com.and2t2.secondhand.domain.repository.NotifikasiRepo
 
-class NotifikasiViewModel(notifikasiRepo: NotifikasiRepo) : ViewModel() {
-
-    val notifikasi = notifikasiRepo
-        .getNotif("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFmaWZAbWFpbC5jb20iLCJpYXQiOjE2NTU0NzkxMzd9.NEn3MajCccdWpLkHiAFAhez3DaFEPIdor7-MDxG9HoE")
-        .asLiveData()
-}
+//class NotifikasiViewModel(notifikasiRepo: NotifikasiRepo) : ViewModel() {
+//
+//    val notifikasi = notifikasiRepo
+//        .getNotif("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFmaWZAbWFpbC5jb20iLCJpYXQiOjE2NTU0NzkxMzd9.NEn3MajCccdWpLkHiAFAhez3DaFEPIdor7-MDxG9HoE")
+//        .asLiveData()
+//}
