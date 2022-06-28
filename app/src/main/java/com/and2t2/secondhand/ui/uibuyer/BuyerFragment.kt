3fff3b0e9b6decd114ebb0d0660202b8e633e3e2
Afif Fujia Rahman <afiffujiarahman2000@gmail.com>
2022-06-28 17:05:23 +0700

    Membuat ui bottom sheet dialog

--- app/src/main/java/com/and2t2/secondhand/ui/uibuyer/BuyerFragment.kt
+++ app/src/main/java/com/and2t2/secondhand/ui/uibuyer/BuyerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.and2t2.secondhand.R
 import com.and2t2.secondhand.common.toRp
 import com.and2t2.secondhand.data.local.DatabaseSecondHand
 import com.and2t2.secondhand.data.remote.ApiClient
@@ -14,6 +15,7 @@ import com.and2t2.secondhand.databinding.FragmentBuyerBinding
 import com.and2t2.secondhand.domain.model.BuyerProductMapper
 import com.and2t2.secondhand.domain.repository.BuyerRepo
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 class BuyerFragment : Fragment() {
@@ -43,6 +45,7 @@ class BuyerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.hide()
         setData(108)
+        onNegoButtonClicked()
     }
 
     private fun  setData(id : Int){
@@ -57,6 +60,19 @@ class BuyerFragment : Fragment() {
             }
             val isTrue: Boolean = it.data?.id == null
             binding.pbLoading.isVisible =  isTrue
+        }
+    }
+
+    private fun onNegoButtonClicked(){
+        binding.btnNego.setOnClickListener {
+            // on below line we are creating a new bottom sheet dialog.
+            val dialog = BottomSheetDialog(requireContext())
+
+            // on below line we are inflating a layout file which we have created.
+            val view = layoutInflater.inflate(R.layout.buyer_10, null)
+
+            dialog.setContentView(view)
+            dialog.show()
         }
     }
 
