ddfdb330641635e4df1430cd67b3581f4edf362f
melur.puspita1 <melur.puspita1@gmail.com>
2022-07-01 14:00:48 +0700

    edit layout seller 25 - seller 30

--- app/src/main/java/com/and2t2/secondhand/ui/uiseller/InfoPenawarFragment.kt
+++ app/src/main/java/com/and2t2/secondhand/ui/uiseller/InfoPenawarFragment.kt
@@ -1,60 +1,123 @@
 package com.and2t2.secondhand.ui.uiseller
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.navigation.findNavController
 import com.and2t2.secondhand.R
+import com.and2t2.secondhand.common.showSnackbar
+import com.and2t2.secondhand.databinding.FragmentInfoPenawarBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InfoPenawarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InfoPenawarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding : FragmentInfoPenawarBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_penawar, container, false)
+    ): View {
+        _binding = FragmentInfoPenawarBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InfoPenawarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InfoPenawarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        acceptanceButtonOnPressed()
+        hubungiButtonOnPressed()
+        statusButtonOnPressed()
+        backButtonOnPressed()
+    }
+
+    private fun acceptanceButtonOnPressed() {
+        binding.btnTerima.setOnClickListener {
+            hubungiDialog()
+            binding.sudahDiterima.isVisible = true
+            binding.belumDiterima.isVisible = false
+        }
+
+        binding.btnTolak.setOnClickListener {
+            binding.belumDiterima.isVisible = false
+            binding.sudahDiterima.isGone = true
+            binding.divider1.isVisible = true
+
+            showSnackbar(requireContext(), requireView(), "test", R.color.success)
+
+            binding.tvProductBid.apply {
+                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                text = "Striked thru text"
             }
+        }
+//        binding.tvName.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+    }
+
+    private fun hubungiDialog() {
+        // on below line we are creating a new bottom sheet dialog.
+        val dialog = BottomSheetDialog(requireContext())
+
+        // on below line we are inflating a layout file which we have created.
+        val view = layoutInflater.inflate(R.layout.seller_30, null)
+
+        dialog.setContentView(view)
+        dialog.show()
+    }
+
+    private fun hubungiButtonOnPressed() {
+        binding.btnHubungi.setOnClickListener {
+            hubungiDialog()
+        }
+    }
+
+    private fun statusButtonOnPressed() {
+        binding.btnStatus.setOnClickListener {
+            // on below line we are creating a new bottom sheet dialog.
+            val dialog = BottomSheetDialog(requireContext())
+
+            // on below line we are inflating a layout file which we have created.
+            val view = layoutInflater.inflate(R.layout.seller_28, null)
+
+            dialog.setContentView(view)
+            dialog.show()
+
+            val buttonKirim = dialog.findViewById<Button>(R.id.btn_set_status)
+            val buttonSuccess = dialog.findViewById<RadioButton>(R.id.radio_success)
+            val buttonCancel = dialog.findViewById<RadioButton>(R.id.radio_cancel)
+
+
+            buttonSuccess?.setOnClickListener{
+                buttonKirim?.isEnabled = true
+            }
+
+            buttonCancel?.setOnClickListener{
+                buttonKirim?.isEnabled = true
+            }
+
+            buttonKirim?.setOnClickListener {
+                showSnackbar(requireContext(), requireView(), "Status produk", R.color.success)
+                dialog.dismiss()
+            }
+        }
+    }
+
+    private fun backButtonOnPressed() {
+        binding.backBtn.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
     }
 }
