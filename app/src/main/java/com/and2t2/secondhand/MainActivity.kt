7723be3358c6777c26597bdc54a474e05c017b1b
melur.puspita1 <melur.puspita1@gmail.com>
2022-07-07 13:47:20 +0700

    menambahkan alert dialog jika belum login

--- app/src/main/java/com/and2t2/secondhand/MainActivity.kt
+++ app/src/main/java/com/and2t2/secondhand/MainActivity.kt
@@ -3,10 +3,7 @@ package com.and2t2.secondhand
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -36,20 +33,6 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-                if (destination.id == R.id.navigation_notifikasi ||
-                    destination.id == R.id.navigation_jual ||
-                    destination.id == R.id.navigation_daftarjual ||
-                    destination.id == R.id.navigation_akun ||
-                    destination.id == R.id.buyerFragment
-                ) {
-                    datastoreViewModel.getLoginState().observe(this) {
-                        if (!it) {
-//                            Toast.makeText(this, "Anda belum masuk", Toast.LENGTH_SHORT).show()
-                            showAlertDialogWithAction()
-//                            startActivity(Intent(this, AuthActivity::class.java))
-                        }
-                    }
-                }
                 if (destination.id == R.id.profile ||
                     destination.id == R.id.navigation_jual ||
                     destination.id == R.id.previewProdukFragment ||
@@ -60,21 +43,31 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     binding.bottomNavigationView.visibility = View.VISIBLE
                 }
+            if (destination.id == R.id.navigation_notifikasi ||
+                destination.id == R.id.navigation_jual ||
+                destination.id == R.id.navigation_daftarjual ||
+                destination.id == R.id.navigation_akun ||
+                destination.id == R.id.buyerFragment
+            ) {
+                datastoreViewModel.getLoginState().observe(this) {
+                    if (!it) {
+                        navController.navigate(R.id.navigation_home)
+                        showAlertDialogWithAction()
+                    }
+                }
+            }
         }
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     private fun showAlertDialogWithAction() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
             val dialog = AlertDialog.Builder(this)
             dialog.setMessage("Anda harus login terlebih dahulu")
             dialog.setPositiveButton("Login") { dialogInterface, angka ->
                 startActivity(Intent(this, AuthActivity::class.java))
             }
             dialog.setNegativeButton("Batal") { dialogInterface, _ ->
-                navController.navigate(R.id.navigation_home)
+
             }
             dialog.setCancelable(false)
             dialog.show()
