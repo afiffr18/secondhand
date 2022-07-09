b0d185659826c86caa61bd6fec1bc9c8be63cb38
Aji Fauzi P <ajifauzi919@gmail.com>
2022-07-09 16:19:38 +0700

    .

--- app/src/main/java/com/and2t2/secondhand/ui/uiseller/uidaftarjual/diminati/Diminati.kt
+++ app/src/main/java/com/and2t2/secondhand/ui/uiseller/uidaftarjual/diminati/Diminati.kt
@@ -1,6 +1,5 @@
 package com.and2t2.secondhand.ui.uiseller.uidaftarjual.diminati
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
