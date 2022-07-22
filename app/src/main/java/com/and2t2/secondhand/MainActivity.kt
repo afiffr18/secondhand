c91972e338e8aa7c308ee56f223b168cec88cd97
melur.puspita1 <melur.puspita1@gmail.com>
2022-07-22 18:22:18 +0700

    menambahkan logic untuk menghapus data pada saat belum login

--- app/src/main/java/com/and2t2/secondhand/MainActivity.kt
+++ app/src/main/java/com/and2t2/secondhand/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.and2t2.secondhand.common.viewModelsFactory
 import com.and2t2.secondhand.databinding.ActivityMainBinding
+import com.and2t2.secondhand.domain.repository.CommonRepo
 import com.and2t2.secondhand.domain.repository.DatastoreManager
 import com.and2t2.secondhand.domain.repository.DatastoreViewModel
+import com.and2t2.secondhand.ui.uiakun.AkunViewModel
 import com.and2t2.secondhand.ui.uinotifikasi.NotifikasiViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,13 +26,24 @@ class MainActivity : AppCompatActivity() {
 
     private val notifikasiViewModel : NotifikasiViewModel by viewModel()
 
+    private val commonRepo: CommonRepo by lazy { CommonRepo(this) }
+    private val akunViewModel: AkunViewModel by viewModelsFactory { AkunViewModel(commonRepo) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         setBottomNav()
-        addNotifikasiBadge()
+        observeData()
+    }
+
+    private fun observeData() {
+        datastoreViewModel.getLoginState().observe(this) {
+            if (it) {
+                addNotifikasiBadge()
+            }
+        }
     }
 
     private fun setBottomNav() {
@@ -50,6 +63,7 @@ class MainActivity : AppCompatActivity() {
                 if (destination.id != R.id.navigation_home) {
                     datastoreViewModel.getLoginState().observe(this) {
                         if (!it) {
+                            akunViewModel.deleteTable()
                             showAlertDialogWithAction()
                         } else {
                             if (destination.id == R.id.navigation_jual) {
